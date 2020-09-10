e453ad453c1db23cfd84972c6b08c4b46d7e2047
igatai <taiti0614@gmail.com>
2020-09-11 00:40:36 +0900

    commit

--- backup2nas.sh
+++ backup2nas.sh
@@ -5,53 +5,43 @@
 ### # # #  #  #  #    #    #    #        #                #                               #
 
 ### This script Searches nas ip address with correct mac address of nas from mac book, mount to nas, and sync backup files.
-### * nas product name is HDL-AAX2 producted by io-data.
+### * Backup data : Documents, codes, Pictures, Music and Movies.
+### * Backup destination : Directory to share with family on NAS.
+### * NAS Product information : HDL-AAX2 / io-data.
 
-#set -e
 
-### store a value in variable.
+### Store a value in variable.
 
-## Shell directory
+# Set secret information from other file to environmental valiables.(nas_mac_address,nas_user,nas_password)
+source ./secret.sh
+
+## Shell directory.
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-echo ${SCRIPT_DIR}
 
-## network address.
+## Network address.
 seg_nw="192.168.0"
 
-## nas mac address. 
-mac_add_nas="34:76:c5:ee:8:3b"
-
-## nas ip address.
+## Nas ip address.
 ip_add_nas=""
 
-## nas user.
-nas_user="taichi"
-
-## password for nas user.
-password="xxxxxxxxxx"
-
-## nas directory to be mounted on nas.
+## Directory to be mounted on nas.
 mounted_dir="t_data"
 
-## home directory
+## Home directory.
 home_dir="/Users/Taiti"
 
-## mount directory path on pc.
-mount_dir="${home_dir}/mnt/nas_iodata"
+## Mount directory on pc.
+mount_dir="${home_dir}/mnt/nas_iodata_1"
 
-## backup source directory on pc.
+## Backup source directory on pc.
 src_dir1="Documents"
 src_dir2="projects"
 src_dir3="Pictures"
 src_dir4="Music"
 src_dir5="Movies"
 
-## backup source directory path on pc.
-#src_dir_path="/Users/Taiti"
-
-## backup destination directory path on nas
+## Destination directory to backup data on nas.
 dst_dir="${mount_dir}/macbook_backup"
-
 
 ### Find nas IP Address from network segment.
 
@@ -62,18 +52,17 @@ for ip_address in `seq 1 254`;do
   mac_add=$(ping -c 1 -W 0.5 ${seg_nw}.${ip_address} > /dev/null && arp ${seg_nw}.${ip_address} | cut -d " " -f 4 2>&1)
 
   ## Compare mac address with one of nas, get ip address of nas.
-  if [ "${mac_add}" = "${mac_add_nas}" ]; then
+  if [ "${mac_add}" = "${nas_mac_address}" ]; then
      ip_add_nas="${seg_nw}.${ip_address}"
      #echo ${ip_add_nas}
      break
   fi;
 done
 
-### mount to nas.
-mount -t smbfs -w //${nas_user}:${password}@${ip_add_nas}/${mounted_dir} ${mount_dir}
- # ex) mount -t smbfs //taichi:baggio1981id@192.168.0.105/t_data ~/mnt/nas_iodata
+### Mount to nas.
+mount -t smbfs -w //${nas_user}:${nas_password}@${ip_add_nas}/${mounted_dir} ${mount_dir}
 
-### sync data.
+### Sync data.
 
 echo "rsync -ahv ${home_dir}/${src_dir3}/ ${dst_dir}/ > ${SCRIPT_DIR}/rsync_${src_dir3}.log &"
 rsync -ahv --delete ${home_dir}/${src_dir1}/ ${dst_dir}/${src_dir1}/ > ${SCRIPT_DIR}/rsync_${src_dir1}.log &
@@ -84,4 +73,3 @@ rsync -ahv --delete ${home_dir}/${src_dir5}/ ${dst_dir}/${src_dir5}/ > ${SCRIPT_
 
 ### unmount nas
 umount ${mount_dir}
- # ex)umount /Users/Taiti/mnt/nas_iodata
---
+++ secret.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Set secret value to environmental valiables.
+
+export nas_mac_address="34:76:c5:ee:8:3b"
+export nas_user="igat"
+export nas_password="pekepeke"
